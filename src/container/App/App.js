0df230857ea1a9284cc2cb3f83733892ed360d4b
Peter Chen <peterchen2017125@gmail.com>
2022-12-26 20:20:16 +0800

    20221226

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -8,17 +8,26 @@ import {
   StyledExploerButton,
   StyledSignInButton
 } from './App.Styled';
+import About from '../About/About';
+import Recipes from '../Recipes/Recipes';
 
 function App() {
   // const [isSignInClick, setSignInClick] = React.useState(false)
   const [pathname, setPathname] = React.useState('/')
   const [searchStart, setSearchStart] = React.useState(false)
+
+  React.useEffect(() => {
+    const pn = window.location.pathname
+    setPathname(pn)
+  }, [])
   return (
 
     <StyledAppHeader>
       <StyledAppHeaderNavigationBar>
         <div>
+          <a href='/'>
           <img src={Logo} />
+          </a>
         </div>
         <div className='navigation-list'>
           <div className='search'>
@@ -27,10 +36,10 @@ function App() {
           </div>
           <div className='navigation'>
             <ul>
-              <li>Recipes</li>
+              <li><a href='/recipes'>Recipes</a></li>
               <li>Dinner TV</li>
               <li>Cooking School</li>
-              <li>About</li>
+              <li><a href='/about'>About</a></li>
             </ul>
           </div>
           <div>
@@ -48,6 +57,12 @@ function App() {
             </div>
             <StyledExploerButton>Explore Recipes</StyledExploerButton>
           </React.Fragment>
+        }
+        {
+          pathname === '/about' && <About></About>
+        }
+        {
+          pathname === '/recipes' && <Recipes></Recipes>
         }
       </StyledAppHeaderContent>
     </StyledAppHeader>
---
+++ src/container/Recipes/Recipes.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Recipes = ()=>{
+    return <h2>Welcome Recipes</h2>
+}
+
+export default Recipes
